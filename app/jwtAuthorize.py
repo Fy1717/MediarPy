83db3528f89e403fda45c3de9b53ada647c44905
Furkan Yıldız <40992809+Fy1717@users.noreply.github.com>
2023-04-02 06:20:48 +0300

    jwt authorization updated

--- app/jwtAuthorize.py
+++ app/jwtAuthorize.py
@@ -6,66 +6,6 @@ from app.models import User
 queries = User()
 
 
-""" def login_required(f):
-    @wraps(f)
-    def decorator(*args, **kwargs):
-        if "token" in session or "token" in request.headers:
-            if "token" in session:
-                print("-------token in session-------")
-                token = session["token"]
-                print("token session --> ", token)
-
-            if "token" in request.headers:
-                print("-------token in headers-------")
-                token = request.headers["token"]
-                print("token headers --> ", token)
-        else:
-            session["token"] = "None"
-            token = session["token"]
-
-        if token == "None":
-            print("------- token none -------------")
-            return (
-                jsonify({"success": False, "error": "Please login the system.."}),
-                401,
-            )
-
-        try:
-            if jwt.decode(token, "mediar-secret", algorithms=["HS256"]) and ("token" in request.headers):
-                token = request.headers["token"]
-                dataDef = jwt.decode(token, "mediar-secret-def")
-
-                if dataDef["id"] == "mediar-secret-def":
-                    current_user = {"id": 0, "name": "mediar"}
-
-            if jwt.decode(token, "mediar-secret", algorithms=["HS256"]):
-                data = jwt.decode(token, "mediar-secret", algorithms=["HS256"])
-                current_user = queries.getOneUser(id=data["id"])
-                print("id: ", current_user.id,
-                      "\nusername: ", current_user.username)
-
-            if current_user == None:
-                return (
-                    jsonify(
-                        {"success": False, "error": "Please login the system.."}),
-                    401,
-                )
-        except Exception as e:
-            return (
-                jsonify(
-                    {
-                        "success": False,
-                        "error": str(e) + " - Signature has expired, please login the system..",
-                    }
-                ),
-                401,
-            )
-
-        return f(current_user, *args, **kwargs)
-
-    return decorator """
-
-
 def login_required(f):
     @wraps(f)
     def decorator(*args, **kwargs):
@@ -86,7 +26,8 @@ def login_required(f):
 
         if token == "None":
             print("------- token none -------------")
-            error_message = {"success": False, "error": "Please login the system.."}
+            error_message = {"success": False,
+                             "error": "Please login the system.."}
             return jsonify(error_message), 401
 
         try:
@@ -107,7 +48,8 @@ def login_required(f):
             return jsonify(error_message), 401
 
         if current_user is None:
-            error_message = {"success": False, "error": "Please login the system.."}
+            error_message = {"success": False,
+                             "error": "Please login the system.."}
             return jsonify(error_message), 401
 
         return f(current_user, *args, **kwargs)
